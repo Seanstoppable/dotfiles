cee453559faffdf205c707e4158cb3851cc2672a
Sean Smith <seandanielsmith@gmail.com>
2015-12-19 12:28:10 -0500

    Fix up neovim setup

--- setup.sh
+++ setup.sh
@@ -8,9 +8,8 @@ ln -sf ~/.dotfiles/vim ~/.vim
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 cp ~/.dotfiles/gitconfig.global ~/.gitconfig
 
-./brew.sh
 ./osxsetup.sh
 
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-ln -s ~/.vim $XDG_CONFIG_HOME/nvim
-ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+ln -s ~/.dotfiles/vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.dotfiled/vimrc $XDG_CONFIG_HOME/nvim/init.vim
