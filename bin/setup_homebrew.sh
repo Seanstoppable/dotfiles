8b0c302cf523f02fdaf4c866998136fc35d0d56a
Sean Smith <ssmith@securityscorecard.io>
2015-12-19 11:51:33 -0500

    Centralize brew install

--- bin/setup_homebrew.sh
+++ bin/setup_homebrew.sh
@@ -1,10 +1,15 @@
 #install brew is not installed
 command -v brew >/dev/null 2>&1 || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ; }
 
-#some essential commands
-brew install ctags
-brew install git
-brew install the_silver_searcher
-brew install vim
-brew install watch
-brew install wget
+apps=(
+  ag
+  awscli
+  git
+  neovim/neovim/neovim
+  rbenv
+  vim
+  watch
+  wget
+)
+
+brew install ${apps[@]}
