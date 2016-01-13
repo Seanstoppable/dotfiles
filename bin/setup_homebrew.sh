7602ab8be510a2a7c13b36db75d053b9860fe48f
Sean <seandanielsmith@gmail.com>
2016-01-13 16:07:42 -0500

    Update homebrew installer
    Add private tap
    Add awsshell

--- bin/setup_homebrew.sh
+++ bin/setup_homebrew.sh
@@ -1,8 +1,11 @@
 #install brew is not installed
 command -v brew >/dev/null 2>&1 || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ; }
 
-apps=(
+brew tap Seanstoppable/random 2> /dev/null
+
+brew_apps=(
   awscli
+  awsshell
   bash-completion
   git
   neovim
@@ -14,8 +17,8 @@ apps=(
 )
 
 
-MISSING=$(comm -1 -3 <(brew list) <(for X in "${apps[@]}"; do echo "${X}"; done|sort))
-INSTALLED=$(comm -1 -2 <(brew list) <(for X in "${apps[@]}"; do echo "${X}"; done|sort) | tr '\n' ' ')
+MISSING=$(comm -1 -3 <(brew list) <(for X in "${brew_apps[@]}"; do echo "${X}"; done|sort))
+INSTALLED=$(comm -1 -2 <(brew list) <(for X in "${brew_apps[@]}"; do echo "${X}"; done|sort) | tr '\n' ' ')
 
 if [[ ! -z ${MISSING} ]]; then
   brew install ${MISSING}
