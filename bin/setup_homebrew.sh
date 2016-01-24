5a9006341d1a53b8e9c256c54d7af43ad8d66fdf
Sean <seandanielsmith@gmail.com>
2016-01-24 12:04:42 -0500

    Add bitbar

--- bin/setup_homebrew.sh
+++ bin/setup_homebrew.sh
@@ -2,6 +2,7 @@
 command -v brew >/dev/null 2>&1 || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ; }
 
 brew tap Seanstoppable/random 2> /dev/null
+brew tap caskroom/cask 2> /dev/null
 
 brew_apps=(
   awscli
@@ -16,6 +17,7 @@ brew_apps=(
   wget
 )
 
+brew cask install bitbar 2> /dev/null
 
 MISSING=$(comm -1 -3 <(brew list) <(for X in "${brew_apps[@]}"; do echo "${X}"; done|sort))
 INSTALLED=$(comm -1 -2 <(brew list) <(for X in "${brew_apps[@]}"; do echo "${X}"; done|sort) | tr '\n' ' ')
@@ -25,5 +27,5 @@ if [[ ! -z ${MISSING} ]]; then
 fi
 
 if [[ ! -z ${INSTALLED} ]]; then
-  brew upgrade ${INSTALLED}
+  brew upgrade ${INSTALLED} 2> /dev/null
 fi
---
+++ bitbar/brew-updates.1h.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+####
+# List available updates from Homebrew (OS X)
+###
+
+exit_with_error() {
+  echo "err | color=red";
+  exit 1;
+}
+
+/usr/local/bin/brew update > /dev/null || exit_with_error;
+
+UPDATES=$(/usr/local/bin/brew outdated --verbose);
+
+UPDATE_COUNT=$(echo "$UPDATES" | grep -c '[^[:space:]]');
+
+echo "↑$UPDATE_COUNT | dropdown=false"
+echo "---";
+if [ -n "$UPDATES" ]; then
+  echo "Upgrade all | bash=/usr/local/bin/brew param1=upgrade"
+  echo "$UPDATES | refresh=true" | awk '{print $0 " | bash=/usr/local/bin/brew param1=upgrade param2=" $1 }'
+fi
