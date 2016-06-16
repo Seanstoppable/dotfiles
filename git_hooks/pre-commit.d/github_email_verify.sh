906ed74b47b1ad66efb7f0d6ac4e7e7001e11421
Sean Smith <seandanielsmith@gmail.com>
2016-06-16 14:45:22 -0400

    Add new git hook intended to prevent using wrong email when committing to github

---
+++ git_hooks/pre-commit.d/github_email_verify.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+#Totally stolen from https://github.com/kintoandar/git-hooks
+
+LOCAL_NAME=$(git config --local user.name)
+LOCAL_EMAIL=$(git config --local user.email)
+
+GLOBAL_NAME=$(git config --global user.name)
+GLOBAL_EMAIL=$(git config --global user.email)
+
+GITHUB=$(git config --get remote.origin.url | grep -c github.com)
+
+if [ "$GITHUB" -eq 1 ] && [[ -z "$LOCAL_EMAIL" ]]; then
+  # assigns stdin
+  exec < /dev/tty
+
+  printf \
+"[WARN] Trying to commit to a github repo using global settings
+Global git config
+\tuser.name:\t%s
+\tuser.email:\t%s\n\n" \
+"$GLOBAL_NAME" "$GLOBAL_EMAIL"
+
+  read -p "[WARN] Use global email [$GLOBAL_EMAIL] for this repo (y/N): " -r OPTION
+
+  if [[ $OPTION == [yY] ]]; then
+    git config --local user.email "$GLOBAL_EMAIL"
+    printf "\n[OK] Please run the commit again\n"
+    exit 1
+  else
+    
+    if [[ ! -z "$PRE_COMMIT_GITHUB_EMAIL" ]]; then
+      read -p "Enter new local user.email or leave blank to proceed with [$PRE_COMMIT_GITHUB_EMAIL]: " -r NEW_EMAIL
+      if [[ -z "$NEW_EMAIL" ]]; then
+        NEW_EMAIL="$PRE_COMMIT_GITHUB_EMAIL"
+      fi
+    else 
+      read -p "Enter new local user.email: " -r NEW_EMAIL
+    fi
+
+    read -p "Proceed with new local user.email [$NEW_EMAIL] (Y/n): " -r OPTION
+
+    case $OPTION in
+      [nN]* ) printf "\n[FATAL] Aborted by user!\n" && \
+              exit 1;;
+          * ) git config --local user.email "$NEW_EMAIL" && \
+              printf "\n[OK] Please run the commit again\n" && \
+              exit 1;;
+    esac
+  fi
+fi
+
+exit 0
