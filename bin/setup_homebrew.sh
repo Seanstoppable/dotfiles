71462eaea34f9e53d603f392b1d6f96645051ed8
Sean Smith <seandanielsmith@gmail.com>
2016-11-06 22:31:06 -0500

    Make homebrew setup a proper script

--- bin/setup_homebrew.sh
+++ bin/setup_homebrew.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 #install brew is not installed
 command -v brew >/dev/null 2>&1 || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ; }
 
