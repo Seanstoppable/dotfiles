7d7de30dc4ca44745e59a8f4764c4713a851b50f
Sean Smith <seandanielsmith@gmail.com>
2016-12-08 00:03:30 -0500

    Clean up osxsetup slightly
    Proper shell directive
    Extra comments
    Disable IR Controller

--- osxsetup.sh
+++ osxsetup.sh
@@ -1,3 +1,9 @@
+#/usr/bin/env sh
+
+#show all files in finder
 defaults write com.apple.finder AppleShowAllFiles YES
 killall Finder
+
+#disable IR Controller
+sudo defaults write /Library/Preferences/com.apple.driver.AppleIRController DeviceEnabled -bool false
 
