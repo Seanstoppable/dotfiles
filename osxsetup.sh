6cbe703b02a933c9f4245f98df1560eb9e8c2052
Sean Smith <seandanielsmith@gmail.com>
2017-04-19 19:41:13 -0400

    Add new command line tool install

--- osxsetup.sh
+++ osxsetup.sh
@@ -7,3 +7,5 @@ killall Finder
 #disable IR Controller
 sudo defaults write /Library/Preferences/com.apple.driver.AppleIRController DeviceEnabled -bool false
 
+#install command line devtools
+xcode-select --install
