35bbad926348da8f5d68b1b984b160bd500a3629
Sean Smith <seandanielsmith@gmail.com>
2018-06-11 23:26:40 -0400

    Add root dotfiles directory
    And verbosity flag

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DOTFILES_HOME="$HOME/.dotfiles"
+VERBOSE=false
+
 mkdir -p ~/bin
 
 files=(
@@ -13,17 +16,21 @@ files=(
 for item in ${files[*]}; do
   CANDIDATE="$HOME/.$item"
   if [ ! -f "$CANDIDATE" ] && [ ! -d "$CANDIDATE" ]; then
-    ln -s "$HOME/.dotfiles/$filename" "$CANDIDATE"
+    ln -s "$DOTFILES_HOME/$filename" "$CANDIDATE"
   else
-    echo "$CANDIDATE already exists, skipping"
+    if [ $VERBOSE = true ]; then
+      echo "$CANDIDATE already exists, skipping"
+    fi
   fi
 done
 
 GITCONFIG=~/.gitconfig
 if [ ! -f "$GITCONFIG" ]; then
-  cp ~/.dotfiles/gitconfig.global "$GITCONFIG"
+  cp "$DOTFILES_HOME/gitconfig.global $GITCONFIG"
 else
-  echo "$GITCONFIG already exists, skipping"
+  if [ $VERBOSE = true ]; then
+    echo "$GITCONFIG already exists, skipping"
+  fi
 fi
 
 #create .config groups if they exist
@@ -31,17 +38,19 @@ cd .config || exit
 for filename in *; do
   CANDIDATE="$HOME/.config/$filename"
   if [ ! -f "$CANDIDATE" ] && [ ! -d "$CANDIDATE" ]; then
-    ln -s "$HOME/.dotfiles/.config/$filename $CANDIDATE"
+    ln -s "$DOTFILES_HOME/.config/$filename $CANDIDATE"
   else
-    echo "$CANDIDATE already exists, skipping"
+    if [ $VERBOSE = true ]; then
+      echo "$CANDIDATE already exists, skipping"
+    fi
   fi
 done
 
 ./osxsetup.sh
 
 mkdir -p "${XDG_CONFIG_HOME:=$HOME/.config}"
-ln -s ~/.dotfiles/vim "$XDG_CONFIG_HOME/nvim"
-ln -s ~/.dotfiles/vimrc "$XDG_CONFIG_HOME/nvim/init.vim"
+ln -s "$DOTFILES_HOME/vim $XDG_CONFIG_HOME/nvim"
+ln -s "$DOTFILES_HOME/vimrc $XDG_CONFIG_HOME/nvim/init.vim"
 
 #make sure gpg exists for programs that use it
 if command -v gpg2 >/dev/null 2>&1; then
@@ -50,5 +59,5 @@ fi
 
 mkdir -p ~/.sbt/0.13/plugins
 mkdir -p ~/.sbt/1.0/plugins
-ln -s ~/.dotfiles/sbtplugins.sbt ~/.sbt/0.13/plugins/dotfileplugins.sbt
-ln -s ~/.dotfiles/sbtplugins.sbt ~/.sbt/1.0/plugins/dotfileplugins.sbt
+ln -s "$DOTFILES_HOME/sbtplugins.sbt" ~/.sbt/0.13/plugins/dotfileplugins.sbt
+ln -s "$DOTFILES_HOME/sbtplugins.sbt" ~/.sbt/1.0/plugins/dotfileplugins.sbt
