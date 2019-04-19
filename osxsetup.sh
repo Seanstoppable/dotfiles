a965918c0a1b6f8198219b132ea5d2e427107954
Sean Smith <seandanielsmith@gmail.com>
2019-04-18 21:57:17 -0400

    Make osx setup more resilient

--- osxsetup.sh
+++ osxsetup.sh
@@ -12,7 +12,10 @@ echo "Prompt to disable IR Controller"
 sudo defaults write /Library/Preferences/com.apple.driver.AppleIRController DeviceEnabled -bool false
 
 # Install command line devtools
-xcode-select --install
+xcode-select -p 1>/dev/null
+if [ $? -ne 0 ] ; then
+  xcode-select --install
+fi
 
 # Disable autocorrect
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
@@ -22,3 +25,11 @@ defaults write com.apple.BezelServices kDim -bool false
 
 # Turn off backlight after interval of inactivity
 defaults write com.apple.BezelServices kDimTime -int 1
+
+# Set system hostname
+if [ ! -f ~/.hostname ] ; then
+  echo "Enter hostname: "
+  read hostname
+  scutil --set HostName $hostname
+  echo $hostname > ~/.hostname
+fi
